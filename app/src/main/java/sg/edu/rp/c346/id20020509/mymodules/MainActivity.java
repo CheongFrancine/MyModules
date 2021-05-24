65c67fcfe0768cef0e98c595d79046f6438c8a9e
CheongFrancine <20020509@rp.edu.sg>
2021-05-24 15:13:43 +0800

    2nd commit

--- app/src/main/java/sg/edu/rp/c346/id20020509/mymodules/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20020509/mymodules/MainActivity.java
@@ -2,13 +2,40 @@ package sg.edu.rp.c346.id20020509.mymodules;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Event handling Step 1
+    TextView tvC203;
+    TextView tvC228;
+    TextView tvC328;
+    TextView tvC331;
+    TextView tvC346;
+    TextView tvG953;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Event handling Step 2
+        tvC203 = findViewById(R.id.tvC203);
+        tvC228 = findViewById(R.id.tvC228);
+        tvC328 = findViewById(R.id.tvC328);
+        tvC331 = findViewById(R.id.tvC331);
+        tvC346 = findViewById(R.id.tvC346);
+        tvG953 = findViewById(R.id.tvG953);
+
+        //Event handling Step 3
+        tvC203.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ModuleDetailActivity.class);
+            }
+        });
     }
 }
